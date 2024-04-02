c4501beea7a331dd0be8c6233f2099297ed8a4be
Chay Park <chay.park@mail.utoronto.ca>
2024-04-01 22:33:34 -0400

    update

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -9,35 +9,13 @@
 #### Workspace setup ####
 library(tidyverse)
 
-#### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+# Rename the CSV files
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+#### Clean data ####
+raw_data <- read_csv("inputs/data/Indicators_of_Anxiety_or_Depression_Based_on_Reported.csv")
+
+# eliminate the rows subdivided By State, eliminate the columns Phase, Time Period, Time Period Label
+
 
 #### Save data ####
 write_csv(cleaned_data, "outputs/data/analysis_data.csv")
