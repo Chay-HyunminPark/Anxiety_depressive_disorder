182d6517a70f3fb074a1f653badf9af627229045
Chay Hyunmin Park <123585452+Chay-HyunminPark@users.noreply.github.com>
2024-04-04 19:40:29 -0400

    Update 04-model.R

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,8 +1,8 @@
 #### Preamble ####
 # Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Chay Park
+# Date: 4 April 2024
+# Contact: chay.park@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
