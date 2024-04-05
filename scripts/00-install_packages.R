95314bbe33f60791c6c2843ddfec97fb3897dff8
Chay Park <chay.park@mail.utoronto.ca>
2024-04-05 00:57:46 +0000

    commit

---
+++ scripts/00-install_packages.R
@@ -0,0 +1,25 @@
+#### Preamble ####
+# Purpose: Installs packages needed to run scripts and Quarto document
+# Author: Chay Park
+# Date: 4 April 2024
+# Contact: chay.park@mail.utoronto.ca
+# License: MIT
+
+#### Workspace setup ####
+## Installing packages (only needs to be done once per computer)
+install.packages("tidyverse") # Contains data-related packages
+install.packages("knitr") # To make tables
+install.packages("janitor") # To clean datasets
+install.packages("dplyr")
+install.packages("ggplot2") # To make graphs
+install.packages("usethis") 
+install.packages("arrow") # To allow Parquet files to be create
+install.packages("testthat") # To test
+
+# For models
+install.packages("beepr")
+install.packages("broom")
+install.packages("broom.mixed")
+install.packages("purrr")
+install.packages("rstanarm")
+install.packages("modelsummary")
