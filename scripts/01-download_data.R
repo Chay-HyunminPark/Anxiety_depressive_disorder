f571a7034aea59b94921d08b2a07e8b7b6ac4c16
Chay Hyunmin Park <123585452+Chay-HyunminPark@users.noreply.github.com>
2024-04-04 22:45:32 -0400

    Update 01-download_data.R
    Add a line of explanation

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -14,7 +14,8 @@
 # Step 4) Click a data set with a title "Indicators of Anxiety or Depression Based on Reported Frequency of Symptoms During Last 7 Days"
 # Step 5) Double check the data is provided by NCHS/DHIS and the publisher is National Center for Health Statistics
 # Step 6) Click Export button on the top right to export dataset. Before downloading the data, make sure it's set to download file and export format is in CSV.
+
+# Follow the similar procedure for other dataset
 # Step 7) Search "Lack of Social Connection" 
 # Step 8) Click the dataset and check the data is provided by NCHS/DHIS and the publisher is National Center for Health Statistics
 # Step 9) Download the dataset by exporting it in CSV format
-         
